ad63b862936e0d0c57738188af6de48bb50bc780
Pietro Mineo <pietromineo87@gmail.com>
2022-05-03 19:22:26 +0100

    expenses submission option added. facility to specify which quarter an expense is being uploaded to

--- components/uploaders/FileUploader/FileUploader.js
+++ components/uploaders/FileUploader/FileUploader.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { storage } from "../../../utils/firebase/db";
 import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
@@ -22,7 +22,15 @@ export default function CreateNewExpense({ path, docPathHandler }) {
   const [uploading, setUploading] = useState(false);
   const [file, setFile] = useState(null);
 
-  const slug = kebabCase(formatDatePath());
+  //---NEW
+  const [endpoints, setEndPoints] = useState([]);
+  const [taxQuarterOption, setTaxQuarterOption] = useState(
+    kebabCase(formatDatePath())
+  );
+
+  const slug = taxQuarterOption;
+
+  //const slug = 'qe-apr-30-2022'
   const id = v4();
 
   function setFileHandler(e) {
@@ -57,14 +65,58 @@ export default function CreateNewExpense({ path, docPathHandler }) {
       .catch((err) => toast.error(err));
   }
 
+  //---NEW
+  async function createEndpointDoc(obj) {
+    await setDoc(doc(getFirestore(), `${path}`), obj);
+  }
+
+  //---NEW
+  async function checkEndpointDoc() {
+    const endpointDoc = await getDoc(doc(getFirestore(), `${path}`));
+
+    if (endpointDoc.exists()) {
+      const data = endpointDoc.data();
+
+      if (!data.endpoints) {
+        data["endpoints"] = [{ endpoint: slug }];
+
+        await createEndpointDoc(data);
+      }
+
+      if (data.endpoints) {
+        const endpointData = data.endpoints;
+        setEndPoints(endpointData);
+
+        //---OLD
+        const filteredData = endpointData.filter(
+          (datum) => datum.endpoint === slug
+        );
+        if (filteredData.length === 0) {
+          data.endpoints.push({ endpoint: slug });
+
+          await createEndpointDoc(data);
+        }
+      }
+    }
+
+    return endpointDoc;
+  }
+
+  useEffect(() => {
+    checkEndpointDoc();
+  }, []);
+
   async function createRecord(url) {
     try {
       const ref = doc(getFirestore(), `${path}/${slug}/${id}`);
 
-      async function createEndpointDoc(obj) {
+      //---OLD
+      /* async function createEndpointDoc(obj) {
         await setDoc(doc(getFirestore(), `${path}`), obj);
-      }
+      } */
 
+      //---OLD
+      /*
       const endpointDoc = await getDoc(doc(getFirestore(), `${path}`));
 
       if (endpointDoc.exists()) {
@@ -87,7 +139,10 @@ export default function CreateNewExpense({ path, docPathHandler }) {
             await createEndpointDoc(data);
           }
         }
-      }
+      } */
+
+      //---NEW
+      const endpointDoc = await checkEndpointDoc();
 
       if (!endpointDoc.exists()) {
         const obj = { endpoints: [{ endpoint: slug }] };
@@ -117,6 +172,7 @@ export default function CreateNewExpense({ path, docPathHandler }) {
         {!uploading ? (
           <form onSubmit={uploadFileHandler}>
             <label>Upload Receipt</label>
+
             <input
               type="file"
               onChange={setFileHandler}
@@ -125,6 +181,35 @@ export default function CreateNewExpense({ path, docPathHandler }) {
             <button type="submit" disabled={!file}>
               Upload File
             </button>
+            <div>
+              <label htmlFor="cars">Choose VAT Quarter:</label>
+              <select
+                name="cars"
+                id="cars"
+                onChange={(e) => {
+                  setTaxQuarterOption(e.target.value);
+                  console.log(taxQuarterOption);
+                }}
+                value={kebabCase(formatDatePath())}
+              >
+                {endpoints.map(
+                  (endpoint, idx) =>
+                    endpoint.endpoint && (
+                      <option key={idx} value={endpoint.endpoint}>
+                        {endpoint.endpoint.toUpperCase()}
+                      </option>
+                    )
+                )}
+                {endpoints.some(
+                  (endpoint) =>
+                    !endpoint.endpoint === kebabCase(formatDatePath())
+                ) && (
+                  <option value={kebabCase(formatDatePath())}>
+                    {`${kebabCase(formatDatePath()).toUpperCase()} test`}
+                  </option>
+                )}
+              </select>
+            </div>
           </form>
         ) : (
           <CircularProgress />
